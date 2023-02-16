1e71f066fb7d3acd4f73834edbd74b4170478413
AD001\z004efdb <shreya.malviya@siemens.com>
2023-02-16 14:33:50 +0530

    [Shreya] Add: UCq

---
+++ EmployeeWageCompitation/EmployeeWageCompitation/Emplyee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompitation
+{
+    public class Emplyee
+    {
+        public void Emp()
+        {
+            Console.WriteLine("UC1");
+        }
+    }
+}
---
+++ EmployeeWageCompitation/EmployeeWageCompitation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompitation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emplyee emplyee = new Emplyee();
+            emplyee.Emp();
+            Console.ReadLine();
+        }
+    }
+}
